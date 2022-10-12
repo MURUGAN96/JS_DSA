13e3b8aa38f5524ff8f2794ff0407d11d17612ff
MURUGAN96 <1murugan23@gmail.com>
2022-10-12 18:44:28 +0530

    1 file modified

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ let givenArr = [1, 2, 3, 4, 1, 6, 7];
 const result = {};
 
 let response = false;
+
+//O(logn)
+// for(let i=0; i< nums.length; i++){
+//   if(!result[nums[i]]) result[nums[i]] = 1;
+//     else {
+//       result[nums[i]] = result[nums[i]]+1;
+//       a=true;
+//       break;
+//     }
+// }
+
+//O(n)
 givenArr.map((ele, i) => {
   // result[ele] = result[ele]+1|| 1
   if (!result[ele]) result[ele] = 1;
