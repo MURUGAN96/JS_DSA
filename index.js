f246711a0b82aa8c215f44177f370ff2944b6388
MURUGAN96 <1murugan23@gmail.com>
2022-10-12 18:35:19 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ import './style.css';
 
 const appDiv = document.getElementById('app');
 
-let givenArr = [1, 2, 3, 4, 5, 6, 7];
+let givenArr = [1, 2, 3, 4, 1, 6, 7];
 
-let middle = Math.ceil(givenArr.length / 2);
+const result = {};
 
-appDiv.innerHTML = `<h1>${givenArr.slice(middle, givenArr.length)}</h1>`;
+let response = false;
+givenArr.map((ele, i) => {
+  // result[ele] = result[ele]+1|| 1
+  if (!result[ele]) result[ele] = 1;
+  else {
+    result[ele] = result[ele] + 1;
+    return (response = true);
+  }
+});
+
+appDiv.innerHTML = `<h1>${response}</h1>`;
