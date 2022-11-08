0476ddf1d77e3c0fecc448badd26e6f29959f380
Murugan Athmagnanam <1murugan23@gmail.com>
2022-11-08 17:54:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
-// Import stylesheets
-import './style.css';
+let interval;
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+let throttleTime;
+
+const inputBox = document.querySelector('.input_box');
+const defaultValue = document.querySelector('.default_value');
+const debounceValue = document.querySelector('.debounce_value');
+const throttleValue = document.querySelector('.throttle_value');
+
+inputBox.addEventListener('input', (e)=>{
+    updateValue(e)
+    deBounce(e, 1000);
+    throttle(e, 1000)
+});
+
+function updateValue(e) {
+    defaultValue.textContent = e.target.value;
+}
+function updateDeBounceValue(e) {
+    debounceValue.textContent = e.target.value;
+}
+
+function updateThrottleValue(e) {
+    throttleValue.textContent = e.target.value;
+}
+
+function deBounce(e,delay){
+    if(interval) clearTimeout(interval);
+    interval = setTimeout(()=>{
+        updateDeBounceValue(e)
+    },delay)
+}
+
+function throttle(e,delay){
+    console.log(throttleTime)
+    if(throttleTime) return
+
+    throttleTime = setTimeout(()=>{
+        updateThrottleValue(e);
+        throttleTime = undefined
+    },delay)
+}
